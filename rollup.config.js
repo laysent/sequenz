20ae945fa9f2de13f5fe31986f81767f0c27ec81
LaySent <laysent@hotmail.com>
2016-12-03 00:11:17 +0800

    :bug: fix compile issue for unit test
    `.babelrc` for rollup will influence webpack build process, hence move it inline

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/index.js',
   format: 'cjs',
-  plugins: [babel()],
+  plugins: [babel({
+    babelrc: false,
+    presets: ['es2015-rollup'],
+  })],
   dest: 'lib/index.js',
 };
